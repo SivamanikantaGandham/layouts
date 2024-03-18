fc7e89c80f2625e10f71ad179bad16d128ea2152
Sivamanikanta_Gandham <62531563+SivamanikantaGandham@users.noreply.github.com>
2024-03-18 15:46:38 +0530

    Layouts

---
+++ layouts/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/demo/layouts/layouts/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ layouts/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comdemolayouts/layouts/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
